8466476aa47f902663c3757c9dbbc1a6c4756dac
mislavskii <jornjat@gmail.com>
2024-09-19 15:49:39 +0700

    renamed the core module

--- pages2Text/workers/backupper.py
+++ pages2Text/workers/backupper.py
@@ -1,10 +1,10 @@
-"""Backups a file (imgzip2text.py by default) to backup directory with adding date and time to the file name"""
+"""Backups a file (page2text.py by default) to backup directory with adding date and time to the file name"""
 
 from datetime import datetime as dt
 
 file_name = input('File name: ')
 if not file_name:
-    file_name = '../imgzip2text.py'
+    file_name = '../page2text.py'
 
 with open(file_name, 'r') as f:
     now = str(dt.now()).replace(' ', '_').replace(':', '-').split('.')[0]
